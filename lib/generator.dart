ee4e3eee0cc835e3db3ec2cbceca08b0c7bbbbed
James Aprosail <aprosail@outlook.com>
2025-03-09 22:19:39 +0800

    chore: generator entrypoint

---
+++ lib/generator.dart
@@ -0,0 +1,13 @@
+/// Generator APIs that generate data and stories preview.
+///
+/// ## Incompatibilities with `package:flutter`
+///
+/// This library contains imports of `package:analyzer`,
+/// which is incompatible with `package:flutter`,
+/// so don't import such library into your `package:flutter` related code,
+/// or it will cause compile errors.
+/// And when testing with this library,
+/// `package:flutter_test` and `flutter test` is also not compatible,
+/// that you can only test corresponding code with `package:test`
+/// and the `dart test` command.
+library;
