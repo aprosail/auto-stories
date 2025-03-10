0bba4b12d0e3fe18c40d0fbf81d34efe679810a9
James Aprosail <aprosail@outlook.com>
2025-03-10 22:19:49 +0800

    chore: sync prettier options and license

---
+++ editors/vscode/ci.ts
@@ -0,0 +1,17 @@
+import {copyFileSync} from "node:fs"
+import {join, resolve} from "node:path"
+
+function syncAssets(from: string, to: string, files: string[]) {
+  for (const file of files) copyFileSync(join(from, file), join(to, file))
+}
+
+// Entry point of this script.
+function main() {
+  const root = import.meta.dirname
+  const monorepoRoot = resolve(join(root, "..", ".."))
+
+  // Sync files from monorepo root.
+  copyFileSync(join(monorepoRoot, "LICENSE"), join(root, "license.txt"))
+  syncAssets(monorepoRoot, root, [".prettierrc.yaml", ".prettierignore"])
+}
+main()
