e82342bdc3073dc08bf79d10ca56367b7cef159e
James Aprosail <aprosail@outlook.com>
2025-03-11 23:28:12 +0800

    fix: create output folder before copy

--- lib/src/analysis/package.dart
+++ lib/src/analysis/package.dart
@@ -198,6 +198,7 @@ extension CopyEnsureFile on File {
 extension CopyDirectory on Directory {
   /// Copy all files in the directory to the [newPath].
   void copyAllFilesSync(String newPath) {
+    Directory(newPath).createSync(recursive: true);
     final e = listSync(recursive: true);
     for (final dir in e.whereType<Directory>()) dir.createSync(recursive: true);
     for (final file in e.whereType<File>()) {
