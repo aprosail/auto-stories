dc867b3c0c1f62c97028c403c5d1db831c0e0f3c
James Aprosail <aprosail@outlook.com>
2025-03-11 22:10:54 +0800

    chore: pub ignore example platform code

--- bin/build.dart
+++ bin/build.dart
@@ -9,6 +9,14 @@ Future<void> main(List<String> arguments) async {
       'editors', // Code editor extensions, not a part of the dart package.
       'CONTRIBUTING.md', // Unnecessary for pub.dev, read it on GitHub.
       '*.sh', // Shell scripts for CI/CD.
+      // Unnecessary platform code in the example package.
+      'example/android',
+      'example/ios',
+      'example/linux',
+      'example/macos',
+      'example/test',
+      'example/web',
+      'example/windows',
     ],
   );
 
