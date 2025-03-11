08f5a70d432e0ffbc14f4845fa56b0d56674148b
James Aprosail <aprosail@outlook.com>
2025-03-11 17:35:27 +0800

    fix: install node deps before review

--- review.sh
+++ review.sh
@@ -1,11 +1,4 @@
 # !/bin/bash
-
-# Check root directory.
 dart format --set-exit-if-changed --output none .
 dart analyze --fatal-infos --fatal-warnings
 dart run bin/build.dart test
-
-# Check vscode extension.
-cd editors/vscode
-npm run check
-cd ../..
